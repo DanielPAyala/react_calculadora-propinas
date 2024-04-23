7b262e3b67d9c3325639e6cf40056d98f9cbe1a2
Carlos Daniel <carlosporrasayala@gmail.com>
2024-04-23 03:25:39 -0500

    Add the product database

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,13 @@
 function App() {
-  return <></>;
+  return (
+    <>
+      <header className='bg-teal-400 py-5'>
+        <h1 className='text-center text-4xl font-black'>
+          Calculadora de Propinas y Consumo
+        </h1>
+      </header>
+    </>
+  );
 }
 
 export default App;
